9c595f9361b8993f29e03b0255d546cea1240997
Mehdi Belkhelfa <midou8687@gmail.com>
2025-06-12 16:54:31 +0200

    fix: update JWT secret in application properties and ensure user table mapping

--- Todo_tp_back/src/main/java/org/example/todo_tp_back/model/User.java
+++ Todo_tp_back/src/main/java/org/example/todo_tp_back/model/User.java
@@ -11,6 +11,7 @@ import java.util.Set;
 @Setter
 @NoArgsConstructor
 @AllArgsConstructor
+@Table(name = "\"user\"")
 public class User {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
